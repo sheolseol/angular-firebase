5d000ba1091bcfbe5d15b2d5032dce9afd75812b
sheolseol <siolsiol@hotmail.com>
2018-03-05 22:17:20 -0400

    View create task

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  
+  taskListArray: any[];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.taskService.getAll().snapshotChanges()
+    .subscribe(item => {
+      this.taskListArray = [];
+      item.forEach(element => {
+        let x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.taskListArray.push(x);
+      });
+
+      this.taskListArray.sort((a, b) => {
+        return a.isChecked - b.isChecked;
+      });
+    });
+  }
+
+  addTask(itemTitle) 
+  {
+    this.taskService.create(itemTitle.value);
+    itemTitle.value = null;
+  }
+
+  updateTask($key: string, isChecked: boolean)
+  {
+    this.taskService.update($key, !isChecked);
+  }
+
+  deleteTask($key: string)
+  {
+    if(confirm('Are you sure you want to delete the record?'))
+    {
+      this.taskService.destroy($key);
+    }
   }
 
 }
